58f25dddd237b912b0ec00bd7aa57ac0442aa2fc
dmdqwerty <150854297+dmdqwerty@users.noreply.github.com>
2023-11-19 22:50:47 +0200

    Step 3

--- lab1/src/main/java/org/calc/StringCalculator.java
+++ lab1/src/main/java/org/calc/StringCalculator.java
@@ -1,16 +1,33 @@
 package org.calc;
 
+import java.util.regex.Pattern;
+
 public class StringCalculator {
     public int add(String numbers) {
-        if (numbers.length() == 0) {
+        if (numbers.isEmpty()) {
             return 0;
         }
-
-        String[] numbersSplit = numbers.split(",");
         int result = 0;
-        for (String number : numbersSplit) {
-            result += Integer.parseInt(number);
+        String[] numbersSplitted = splitNumbers(numbers);
+        for (String num : numbersSplitted) {
+            result += Integer.parseInt(num);
         }
         return result;
+    }
+
+    private String[] splitNumbers(String numbers) {
+        String[] numbersSplitted = Pattern.compile("[\n,]").split(numbers);
+        for (String number : numbersSplitted) {
+            if (number.isEmpty()) {
+                throw new IllegalArgumentException("Two delimiters in a row are not allowed.");
+            }
+        }
+        return numbersSplitted;
+    }
+
+    public static void main(String[] args) {
+        StringCalculator calc = new StringCalculator();
+        int res = calc.add("3,7\n20");
+        System.out.println(res);
     }
 }
--- lab1/src/test/java/org/calc/StringCalculatorTest.java
+++ lab1/src/test/java/org/calc/StringCalculatorTest.java
@@ -32,4 +32,14 @@ public class StringCalculatorTest {
         Assertions.assertEquals(320, calc.add("100,10,10,150,50"));
     }
 
+    @Test
+    void newLineAsDelimiterCase() {
+        Assertions.assertEquals(100, calc.add("30,50\n20"));
+    }
+
+    @Test
+    void twoDelimitersInARowCase() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> calc.add("3,5,\n6"), "Two delimiters in a row are not allowed");
+    }
+
 }
